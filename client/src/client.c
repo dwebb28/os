264ecc1e814e613d1aca54eb93fa99536499c946
Spidey <rudwebb2014@gmail.com>
2018-11-01 21:15:47 -0400

    addingin client and server for assignment3 project

---
+++ client/src/client.c
@@ -0,0 +1,91 @@
+/*
+ ============================================================================
+ Name        : client.c
+ Author      : Daniel Webb
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Receive text from the user and send strings to shared memory only if
+ 	 	 	   they include the substring "C00L"
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+
+int main(void) {
+
+	/*
+	 * Initialize Arguments
+	 */
+	size_t input_max = 100;
+	char * input = NULL;
+	struct sockaddr_in socket_address;
+	int created_socket = 0, response;
+	struct sockaddr_in server_address;
+	char buffer[1024] = {0};
+	int port = 10001;
+	while(1){
+
+		input = (char *) malloc(input_max * sizeof(char));
+
+		/*
+		 * Initialize the input char array with null values
+		 */
+		for(int i = 0; i < input_max; i++)
+		{
+			input[i] = '\0';
+		}
+
+		/*
+		 * Get the line of input from STDIN and remove the last char which
+		 * will be a '\n' (newline) character
+		 */
+		printf("\nEnter some text (Ctrl+C to quit): ");
+		getline(&input, &input_max, stdin);
+		input[strlen(input) - 1] = '\0';
+
+		//if C00L is a substirng of the user input, copy it to memory
+		if( strstr(input, "C00L") != NULL ){
+			//memcpy(shared_memory_addr, input, strlen(input) + 1);
+
+		    if ((created_socket = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+		    {
+		        printf("\n Failed to create socket \n");
+		        return -1;
+		    }
+
+		    memset(&server_address, '0', sizeof(server_address));
+
+		    server_address.sin_family = AF_INET;
+		    server_address.sin_port = htons(port);
+
+		    if(inet_pton(AF_INET, "127.0.0.1", &server_address.sin_addr)<=0)
+		    {
+		        printf("\nInvalid address/ Address not supported \n");
+		        return -1;
+		    }
+
+		    if (connect(created_socket, (struct sockaddr *)&server_address, sizeof(server_address)) < 0)
+		    {
+		        printf("\nConnection Failed \n");
+		        return -1;
+		    }
+		    send(created_socket , input , strlen(input) , 0 );
+		    printf("[%s] sent\n", input);
+		    response = read( created_socket , buffer, 1024);
+		    printf("Received %s\n",buffer );
+
+			// detach from the shared memory
+
+			// tell the processor we are done and wait for the response from the processor
+		}else{
+			printf("Not C00L");
+		}
+
+
+	}
+}
---
+++ server/src/server.c
@@ -0,0 +1,100 @@
+/*
+ ============================================================================
+ Name        : server.c
+ Author      : Daniel Webb
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Pull strings out of the shared memory and compute how many digits are in them
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <stdlib.h>
+#include <netinet/in.h>
+
+int main(void) {
+
+	/*
+	 * Initialize Arguments
+	 */
+	FILE * secrets_file;
+	int listen_port = 10001;
+    struct sockaddr_in socket_address;
+    int opt = 1;
+    int addrlen = sizeof(socket_address);
+	int num_count = 0;
+    char buffer[1024] = {0};
+	int socket_file_desc, created_socket, read_file_desc;
+    char *thanks = "Thank you";
+
+	while(1){
+
+		// wait for the receiver to notify us
+	    if ((socket_file_desc = socket(AF_INET, SOCK_STREAM, 0)) == 0)
+	    {
+	        perror("socket creation has failed");
+	        exit(EXIT_FAILURE);
+	    }
+
+	    socket_address.sin_family = AF_INET;
+	    socket_address.sin_addr.s_addr = INADDR_ANY;
+	    socket_address.sin_port = htons( listen_port );
+
+	    // Forcefully attaching socket to the port 8080
+	    if (bind(socket_file_desc, (struct sockaddr *)&socket_address,
+	                                 sizeof(socket_address))<0)
+	    {
+	        perror("bind failed");
+	        exit(EXIT_FAILURE);
+	    }
+	    if (listen(socket_file_desc, 3) < 0)
+	    {
+	        perror("listen");
+	        exit(EXIT_FAILURE);
+	    }
+	    if ((created_socket = accept(socket_file_desc, (struct sockaddr *)&socket_address,
+	                       	   (socklen_t*)&addrlen))<0)
+	    {
+	        perror("accept");
+	        exit(EXIT_FAILURE);
+	    }
+
+	    read_file_desc = read( created_socket , buffer, 1024);
+		printf("%s\n",buffer );
+		send(created_socket , thanks , strlen(thanks) , 0 );
+		printf("Responded to server\n");
+
+		num_count = 0;
+		for( int i = 0; read_file_desc[i] != '\0'; i++){
+			// if C00L starts at the current index, skip ahead of it so the 0's
+			// in C00L are not counted
+			if( strstr(&read_file_desc[i], "C00L") == &read_file_desc[i] ){
+				i = i + 3;
+				continue;
+			}
+			// if the character is a digit, add it to the count
+			if (isdigit(read_file_desc[i])){
+				num_count++;
+			}
+		}
+
+
+		printf("Number of digits: %d\n", num_count);
+
+		// write the number of digits and the full string to file (append only)
+//		if((secrets_file = fopen("secrets.out", "a")) == NULL){
+//			printf("Could not open secrets.out file!\n");
+//			exit(1);
+//		}
+//
+//		//fprintf(secrets_file, "%d - %s\n", num_count, shared_memory_addr);
+//		fclose(secrets_file);
+
+
+
+	}
+}
